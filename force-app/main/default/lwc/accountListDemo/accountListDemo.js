89c487de8073f16c68036bca92c7abfd4f9c99d0
Harshita Varahabhatla <sarat.cse52@gmail.com>
2021-12-02 01:15:13 -0500

    Lwc changes - 1

--- force-app/main/default/lwc/accountListDemo/accountListDemo.js
+++ force-app/main/default/lwc/accountListDemo/accountListDemo.js
@@ -1,6 +1,7 @@
 import { LightningElement,wire } from 'lwc';
-
+import getAccounts from '@salesforce/apex/AccountListHandler.getAccounts';
 
 export default class AccountListDemo extends LightningElement {
-    
+    @wire(getAccounts)
+    accounts
 }
